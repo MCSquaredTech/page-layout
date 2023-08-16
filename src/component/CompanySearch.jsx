078ee44fede2aa3771798d81646ae3e73607db06
Mark Christensen <mark@mcsquared.tech>
2023-08-15 21:43:47 -0600

    DataStore Code

--- src/component/CompanySearch.jsx
+++ src/component/CompanySearch.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CompanySearch = () => {
+  const [ search, setSearch ] =  useState('')
+
+  const handleSearch = (e) => { 
+      const search = e.target.value; 
+
+      setSearch(search); 
+      console.log(search);
+  }
+
   return (
     <>
       <div class="company-content">
         <aside class="search-col">
-            <div class="search-col-header">
+            <div class="search-col-header input-block">
                 <div class="search-input-wrapper">
-                    <input type="text" placeholder="Search" />
+                <input type='text'
+                    className='input-block'
+                    autoComplete='address-line1'
+                    name='company'
+                    value={ search }
+                    size={"45"}
+                    placeholder='Company Name'
+                    onChange={handleSearch}
+                    /> 
                 </div>
             </div>
             <hr  style={{padding: "0"}}/>
