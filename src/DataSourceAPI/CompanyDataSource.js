fb6e7c8fb3f894d12805b0808c5e56f8f38b2660
Mark Christensen <mark@mcsquared.tech>
2023-08-16 08:46:31 -0600

    DataSource Recommit

---
+++ src/DataSourceAPI/CompanyDataSource.js
@@ -0,0 +1,70 @@
+
+
+const mockApiKey = process.env.REACT_APP_MOCKAPI_KEY;
+
+class CompanyDataSource { 
+
+    getCompany = async () => { 
+        try{ 
+            const resp = await fetch(`${mockApiKey}/`);
+            return await resp.json(); 
+        } catch (e) { 
+            console.log('The Fetch Company Request Failed', e);
+        }
+    }
+
+    getCompanyById = async (Company) => { 
+        try { 
+            const resp = await fetch(`${mockApiKey}/${Company.id}`);
+            return await resp.json();
+        } catch (e) { 
+            console.log('The Fetch Company by Id Request Failed', e);
+        }
+    }
+
+    postCompany = async (Company) {
+        try { 
+            const resp = await fetch(`${mockApiKey}`,
+            {
+                method: "POST",
+                header: { 
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(Company)
+            });
+            return await resp.json();
+        } catch (e) { 
+            console.log('The Post Company Request Failed', e);
+        }
+    }
+
+    putCompany = async (Company) => { 
+        try { 
+            const resp = await fetch(`$(mockApiKey)/${Company.id}`,
+            {
+                method: "PUT",
+                header: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(Company)
+            });
+            return await resp.json();
+        } catch (e) { 
+            console.log('The Put Company Request Failed', e);
+
+        }
+    }
+
+    deleteCompany = async (Company) => { 
+        try { 
+            const resp = await fetch(`${mockApiKey}\${Company.id}`,
+            { 
+                method: "Delete"
+            });
+        } catch (e) { 
+            console.log('The Delete Company Request Failed', e);
+        }
+    }
+}
+
+export const CompanyDataSource = new CompanyDataSource(); 
